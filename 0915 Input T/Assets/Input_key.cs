5b5df71d1b716ce22578b1a3733ae9d5989fa3a7
zkem4wkd <62642237+zkem4wkd@users.noreply.github.com>
2020-09-15 17:39:25 +0900

    20.09.15

---
+++ 0915 Input T/Assets/Input_key.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Input_key : MonoBehaviour
+{
+    public Transform Player;
+
+    public float pSpeed = 5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Player = GameObject.Find("Player").GetComponent<Transform>();
+        //Player = gameObject.GetComponent<Transform>();
+        Player = GameObject.FindGameObjectWithTag("pPlayer").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Player.transform.Translate(Vector2.left * pSpeed*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Player.transform.Translate(Vector2.right * pSpeed* Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Player.transform.Translate(Vector2.up * pSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Player.transform.Translate(Vector2.down * pSpeed * Time.deltaTime);
+        }
+       
+
+    }
+}
