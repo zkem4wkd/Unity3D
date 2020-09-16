bf11a323518b616c643d9caad4ab0bd33c9c0642
zkem4wkd <62642237+zkem4wkd@users.noreply.github.com>
2020-09-16 16:43:41 +0900

    20.09.16

---
+++ 0916 Pratice/Assets/StatScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatScript : MonoBehaviour
+{
+    Transform Player;
+    [SerializeField]
+    private int Hp;
+    [SerializeField]
+    private int Atk;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player").GetComponent<Transform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 0916 Pratice/Assets/testScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testScript : MonoBehaviour
+{
+    Transform Player;
+    public float pSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float Horizon = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+
+        Player.transform.Translate(Vector2.right * Horizon*pSpeed * Time.deltaTime);
+        Player.transform.Translate(Vector2.up *Vertical* pSpeed * Time.deltaTime);
+
+
+
+        
+    }
+}
