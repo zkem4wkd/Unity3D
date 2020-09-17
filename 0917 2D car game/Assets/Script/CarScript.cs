6c56fa651c854365924f012fe162cba4869396dc
zkem4wkd <62642237+zkem4wkd@users.noreply.github.com>
2020-09-17 13:24:13 +0900

    20.09.17

---
+++ 0917 2D car game/Assets/Script/CarScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarScript : MonoBehaviour
+{
+    public float Speed = 0f;
+    Vector2 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //this.Speed = 0.1f;
+
+            this.startPos = Input.mousePosition;
+
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 endPos = Input.mousePosition;
+            float swipeLength = (endPos.x - this.startPos.x);
+
+            this.Speed = swipeLength / 500.0f;
+
+            //효과음 재생
+            GetComponent<AudioSource>().Play();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            this.Speed = -0.1f;
+        }
+
+        transform.Translate(this.Speed,0,0);
+        this.Speed *= 0.96f;
+    }
+}
---
+++ 0917 2D car game/Assets/Script/GameProject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameProject : MonoBehaviour
+{
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+        if (length >= 0 )
+        {
+            this.distance.GetComponent<Text>().text = "목표 지점까지" + length.ToString("F2") + "남았습니다.";
+        }
+        else 
+        {
+            this.distance.GetComponent<Text>().text = "도착";
+        }
+
+        
+    }
+}
