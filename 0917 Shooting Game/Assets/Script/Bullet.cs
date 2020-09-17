81601623e793f28128333ca3efa140d72e2122fd
zkem4wkd <62642237+zkem4wkd@users.noreply.github.com>
2020-09-17 17:43:41 +0900

    20.09.17

---
+++ 0917 Shooting Game/Assets/Script/Bullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public Transform playerPosition;
+    Transform bulletPosition;
+
+    public float bulletSpeed = 10f;
+
+    Vector2 startPosition; // 총알이 생성되었을 때의 최초 위치값
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletPosition = gameObject.GetComponent<Transform>();
+
+        bulletPosition.transform.position = playerPosition.position;
+
+        startPosition = playerPosition.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletPosition.transform.Translate(Vector2.up * bulletSpeed * Time.deltaTime);
+
+        if(startPosition.y + 10f < bulletPosition.position.y)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ 0917 Shooting Game/Assets/Script/PlayerMove.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public Rigidbody2D Player;
+    public Transform Ground;
+    public float playerSpeed = 5f;
+    public float playerJump = 3f;
+    public bool SetJump = false;
+    public float playerG;
+    //총알 관리 변수
+    public GameObject bulletPrefab;
+    public GameObject[] bulletNum;
+    //게임 내 총알들을 찾아 최대 갯수에 제한을 주거나 객체를 찾음
+
+    public int maxBullet = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bulletNum = GameObject.FindGameObjectsWithTag("Bullet");
+
+        playerG = Player.position.y - Ground.position.y;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Player.transform.Translate(Vector2.left * playerSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Player.transform.Translate(Vector2.right * playerSpeed * Time.deltaTime);
+        }
+        if (playerG > 0.8f)
+        {
+            SetJump = true;
+        }
+        else
+        {
+            SetJump = false;
+        }
+        if (SetJump == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Player.AddForce(Vector2.up * playerJump, ForceMode2D.Impulse);
+            }
+
+
+
+        }
+        if (Input.GetKeyDown("z") && bulletNum.Length < maxBullet)
+        {
+            Instantiate(bulletPrefab, new Vector2(Player.position.x,Player.position.y+1),Quaternion.identity);
+        }
+
+    }
+    }
