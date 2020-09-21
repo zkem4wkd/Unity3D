1052ebda74072033875ea21031e954f3d8fd2381
zkem4wkd <69830656+zkem4wkd@users.noreply.github.com>
2020-09-21 21:45:13 +0900

    20.09.21

---
+++ Unity2D Project Game/Assets/PortalScript/Portal1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Portal1 : MonoBehaviour
+{
+    GameObject portal;
+    WalkingScript wPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        portal = GameObject.FindGameObjectWithTag("Portal");
+
+
+        wPlayer = GameObject.Find("Player").GetComponent<WalkingScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float distance = Vector2.Distance(portal.transform.position, wPlayer.transform.position);
+
+        if (distance < 0.3f)
+        {
+            SceneManager.LoadScene("GameClear");
+        }
+
+    }
+}
