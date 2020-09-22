4c7757ecc725e3807fefae4c2ecb5cb41117dee1
zkem4wkd <62642237+zkem4wkd@users.noreply.github.com>
2020-09-22 13:00:10 +0900

    20.09.22

---
+++ 0922 Velocity/Assets/Velocity.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Velocity : MonoBehaviour
+{
+    Rigidbody2D player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+
+        //player의  절대속도 설정
+        //player.velocity = new Vector2(1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player.AddForce(Vector2.right * 3);
+        Debug.Log(player.velocity.x);
+
+        if(player.velocity.x > 2)
+        {
+            Debug.Log("deceleration");
+            player.velocity = new Vector2(2, 0);
+        }
+    }
+}
---
+++ 0922 chapter6/Assets/Script/PlayerScript.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    public float Speed = 5f;
+    float maxWalk = 2f;
+    public float jump = 350f;
+    Animator ani;
+
+    SpriteRenderer sprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        ani = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int key = 0;
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            key = 1;
+            sprite.flipX = false;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            key = -1;
+            sprite.flipX = true;
+        }
+        //abs = ()안에 절대값을 구함
+        float speedX = Mathf.Abs(rigid2D.velocity.x);
+
+        //플레이어의 스피드에 제한을 줌
+        if(speedX < maxWalk )
+        {
+            rigid2D.AddForce(transform.right * key * Speed);
+        }
+        //움직이는 방향에 따라 스케일 변경
+        //if(key != 0)
+        //{
+        //    transform.localScale = new Vector3(key, 1, 1);
+        //}
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && rigid2D.velocity.y == 0)
+        {
+            ani.SetTrigger("JMP");
+            rigid2D.AddForce(transform.up * this.jump);
+        }
+
+        //애니메이션 재생속도 관리
+        ani.speed = speedX / 2.0f;
+
+        if(rigid2D.velocity.y == 0)
+        {
+            ani.speed = speedX;
+        }
+        else
+        {
+            ani.speed = 1.0f;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name=="flag")
+        {
+            SceneManager.LoadScene("ClearScene");
+            
+        }
+        
+
+        
+    }
+}
