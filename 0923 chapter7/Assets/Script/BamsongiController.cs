32d60d00789449056546c68dc4057ad9e51bb6eb
zkem4wkd <62642237+zkem4wkd@users.noreply.github.com>
2020-09-23 12:35:01 +0900

    20.09.23

---
+++ 0923 chapter7/Assets/Script/BamsongiController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BamsongiController : MonoBehaviour
+{
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Shoot(new Vector3(0, 200, 2000));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 0923 chapter7/Assets/Script/BamsongiGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class BamsongiGenerator : MonoBehaviour
+{
+    public GameObject bamsongiPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float randomX = Random.Range(18, 24);
+        //float randomY = Random.Range(3.0f, 8.0f);
+        //float randomZ = Random.Range(0f, 4.0f);
+        if(Input.GetMouseButtonDown(0))
+        {
+            //GameObject bamsongi = Instantiate(bamsongiPrefab,new Vector3(randomX,randomY,randomZ),Quaternion.identity) as GameObject;
+            GameObject bamsongi = Instantiate(bamsongiPrefab) as GameObject;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 worldDir = ray.direction;
+            bamsongi.GetComponent<BamsongiController>().Shoot(worldDir.normalized * 2000);
+
+
+        }
+    }
+}
