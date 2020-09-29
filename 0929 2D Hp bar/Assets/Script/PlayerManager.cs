382f002315153f1ad7e3b2cea57aef34c82e6d07
zkem4wkd <62642237+zkem4wkd@users.noreply.github.com>
2020-09-29 16:14:42 +0900

    20.09.29

---
+++ 0929 2D Hp bar/Assets/Script/PlayerManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    public Slider playerHpBar;
+
+    public int maxPlayerHp = 10000;
+    public int playerHp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHp = maxPlayerHp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(playerHp > 0)
+            {
+                playerHp -= 250;
+                playerHpBar.value = playerHp / (maxPlayerHp / 100);
+            }
+            if(playerHp <= 0)
+            {
+                playerHp = 10000;
+                playerHpBar.value = playerHp / (maxPlayerHp / 100);
+            }
+        }
+    }
+}
