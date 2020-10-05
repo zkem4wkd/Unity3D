8870ae57040e75842f030f18c963b70c309c4567
DongJun Lee <69830656+zkem4wkd@users.noreply.github.com>
2020-10-05 18:21:34 +0900

    20.10.05

---
+++ 1005 3D Tool Timeline/Assets/TimeLineController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimeLineController : MonoBehaviour
+{
+    public PlayableDirector p;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            p.gameObject.SetActive(true);
+            p.Play();
+        }
+    }
+}
