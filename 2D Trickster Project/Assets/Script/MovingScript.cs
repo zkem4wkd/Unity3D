b72632d5f3333f1f4b13e0201fd24a4a8bfd0a4d
DongJun Lee <69830656+zkem4wkd@users.noreply.github.com>
2020-10-08 16:35:14 +0900

    20.10.08

---
+++ 2D Trickster Project/Assets/Script/MovingScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingScript : MonoBehaviour
+{
+    public float speed = 3f;
+    Camera camera;
+    Vector2 mousePosition;
+    Rigidbody2D rigid2d;
+    private float xVelocity = 0.0f;
+    bool flag = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            flag = true;
+            mousePosition = Input.mousePosition;
+            mousePosition = camera.ScreenToWorldPoint(mousePosition);
+            Vector2 Shipmove = mousePosition;
+            StartCoroutine(Moving());
+        }
+    }
+    IEnumerator Moving()
+    {
+        float dis = Vector2.Distance(mousePosition, transform.position);
+        transform.position = Vector3.Lerp(transform.position, mousePosition, 0.2f * Time.deltaTime);
+        if (dis < 0.1f)
+        {
+            flag = false;
+        }
+        yield return null;
+    }
+}
