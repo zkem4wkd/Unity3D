a5c72c4df4abcf9a10ce4c44a16a8f0945d5af9b
DongJun Lee <69830656+zkem4wkd@users.noreply.github.com>
2020-10-13 18:17:44 +0900

    20.10.13

--- 2D Trickster Project/Assets/Script/MovingScript.cs
+++ 2D Trickster Project/Assets/Script/MovingScript.cs
@@ -7,35 +7,40 @@ public class MovingScript : MonoBehaviour
     public float speed = 3f;
     Camera camera;
     Vector2 mousePosition;
-    Rigidbody2D rigid2d;
-    private float xVelocity = 0.0f;
-    bool flag = false;
+    Animator ani;
+    Transform player;
     // Start is called before the first frame update
     void Start()
     {
         camera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        ani = GetComponent<Animator>();
+        player = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            flag = true;
-            mousePosition = Input.mousePosition;
-            mousePosition = camera.ScreenToWorldPoint(mousePosition);
-            Vector2 Shipmove = mousePosition;
-            StartCoroutine(Moving());
-        }
     }
-    IEnumerator Moving()
+    private void FixedUpdate()
     {
-        float dis = Vector2.Distance(mousePosition, transform.position);
-        transform.position = Vector3.Lerp(transform.position, mousePosition, 0.2f * Time.deltaTime);
-        if (dis < 0.1f)
+        if (Input.GetMouseButton(0))
         {
-            flag = false;
+            ani.SetBool("Moving", true);
+            mousePosition = Input.mousePosition;
+            mousePosition = camera.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = Vector2.MoveTowards(transform.position, mousePosition, speed * Time.fixedDeltaTime);
+            if(mousePosition.x > transform.position.x)
+            {
+                player.localScale = new Vector3(-3, 3, 1);
+            }
+            else
+            {
+                player.localScale = new Vector3(3, 3, 1);
+            }
         }
-        yield return null;
+        else
+        {
+            ani.SetBool("Moving", false);
+        }
     }
 }
