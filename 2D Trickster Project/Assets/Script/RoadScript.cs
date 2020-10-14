80c7e507bf979d26a26c154671b5d61f192c8dd4
DongJun Lee <69830656+zkem4wkd@users.noreply.github.com>
2020-10-14 18:16:41 +0900

    20.10.14

---
+++ 2D Trickster Project/Assets/Script/RoadScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class RoadScript : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag=="Tile")
+        {
+            collision.gameObject.GetComponent<TileScript>().roadOn = true;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<RoadScript>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 2D Trickster Project/Assets/Script/TileScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileScript : MonoBehaviour
+{
+    GameObject player;
+    public bool roadOn = false;
+    public Tilemap tile;
+    GameObject road;
+    RoadScript rScript;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            rScript.enabled = true;
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (roadOn == true)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.blue, 3.5f);
+
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, Vector3.zero);
+
+
+            if (this.tile = hit.transform.GetComponent<Tilemap>())
+            {
+                this.tile.RefreshAllTiles();
+                int x, y;
+                x = this.tile.WorldToCell(ray.origin).x;
+                y = this.tile.WorldToCell(ray.origin).y;
+                Vector3Int v3int = new Vector3Int(x, y, 0);
+
+                this.tile.SetTileFlags(v3int, TileFlags.None);
+                //this.tile.SetColor(v3int, new Color(255,0,0));
+                tile.color = new Color(255, 0, 0);
+            }
+        }
+    }
+    private void OnMouseExit()
+    {
+        tile.color = new Color(255, 255, 255);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        road = GameObject.FindGameObjectWithTag("Road");
+        rScript = GameObject.FindGameObjectWithTag("Road").GetComponent<RoadScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
