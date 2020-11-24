87dd1909b71a9850128d819edc038aeaa99a8deb
DongJun Lee <69830656+zkem4wkd@users.noreply.github.com>
2020-11-24 11:44:40 +0900

    20.11.24

---
+++ 1124 Network/Assets/Script/PhotonScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonScript : MonoBehaviourPunCallbacks
+{
+    public string version = "1.0";
+    public string nickName = "name";
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        OnLogin();
+    }
+    void OnLogin()
+    {
+        PhotonNetwork.GameVersion = version;
+        PhotonNetwork.NickName = nickName;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected");
+        PhotonNetwork.JoinRandomRoom();
+    }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed");
+        this.CreateRoom();
+    }
+    void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom(null,new RoomOptions { MaxPlayers = 4 });
+    }
+    public override void OnJoinedRoom()
+    {
+        
+    }
+}
---
+++ 1124 Network/Assets/Script/PlayerControll.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerControll : MonoBehaviourPunCallbacks
+{
+    float h, v, r;
+    Transform tr;
+    Animator anim;
+    public float speed = 10.0f;
+    public float rotSpeed = 60.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(photonView.IsMine)
+        {
+            v = Input.GetAxis("Vertical");
+            h = Input.GetAxis("Horizontal");
+
+            Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+            tr.Translate(moveDir * speed * Time.deltaTime);
+
+            r = Input.GetAxis("Mouse X");
+            tr.Rotate(Vector3.up * Time.deltaTime * r * rotSpeed);
+
+            if(Input.GetMouseButton(0))
+            {
+                anim.SetTrigger("Attack");
+            }
+
+        }
+    }
+}
